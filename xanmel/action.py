703a71de73200df716b129d38c05acc127a4fda3
Nick Savchenko <nsavch@gmail.com>
2016-11-25 21:16:01 +0200

    Remove unused file

--- xanmel/action.py
+++
@@ -1,7 +0,0 @@
-class BaseAction(object):
-    # TODO: include list of required/optional properties
-    def __init__(self, **kwargs):
-        self.properties = kwargs
-
-    def run(self):
-        raise NotImplementedError()
