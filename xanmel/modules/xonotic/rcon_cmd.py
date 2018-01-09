b99316d98835a682ff615ad5b72614108ccc8421
Nick Savchenko <nsavch@gmail.com>
2018-01-09 22:09:28 +0200

    Remove log spamming

--- xanmel/modules/xonotic/rcon_cmd.py
+++ xanmel/modules/xonotic/rcon_cmd.py
@@ -70,7 +70,6 @@ class CvarParser(BaseOneLineRegexParser):
     regex = re.compile(rb'^"(\w+)" is "([^"]*)"')
 
     def process(self, data):
-        self.rcon_server.cvars[data.group(1).decode('utf8')] = data.group(2).decode('utf8')
         logger.debug('Set cvar %s to %r', data.group(1).decode('utf8'), data.group(2).decode('utf8'))
 
 
